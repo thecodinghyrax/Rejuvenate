a829754bd8d10deb59e998346e035af4897cba17
Drew Crawford <thecodinghyrax@gmail.com>
2021-12-07 11:58:02 -0600

    updated the db creation file

---
+++ class_tester.py
@@ -0,0 +1,13 @@
+from web_modules.catagory import Catagory
+from tkinter import Tk as tk
+from web_modules.scraper import Scraper
+from database import db
+
+from controller import Controller
+
+
+'''
+This file will be used to test functionality for the other files for now.
+'''
+if __name__ == '__main__':
+  Controller.add_search_names()
--- database/db.py
+++ database/db.py
@@ -34,25 +34,19 @@ def create_tables():
     """ Create all tables for the db
     :returns: Nothing. Message is printed if connection error
     """
-
     sql_create_web_addon_table = """ CREATE TABLE IF NOT EXISTS web_addon (
                                         ID INTEGER PRIMARY KEY AUTOINCREMENT,
                                         esoui_id text NOT NULL,
                                         name text NOT NULL,
                                     ); """
-
     sql_create_local_addon_table = """ CREATE TABLE IF NOT EXISTS local_addon (
                                         ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        esoui_id text DEFAULT "",
+                                        esoui_id text DEFAULT '',
                                         folder_name text NOT NULL,
-                                        web_name text NOT NULL,
+                                        web_name text DEFAULT '',
                                         loacl_version text DEFAULT '0',
                                         web_version text DEFAULT '0',
                                     ); """
-
-
-
-
     # create a database connection
     conn = create_connection()
     if conn is not None:
@@ -62,71 +56,94 @@ def create_tables():
         print("Unable to connect to the database")
 
 
-def insert_addon(addon):
+def insert_web_addon(addon):
     """Create a new addon for table
     :param conn: The SQLite connection object
-    :param addon: A tuple of a addon (name, )
+    :param addon: A tuple of a addon (esoui_id, name)
     :return: addon id
     """
     conn = create_connection()
     with conn:
         cur = conn.cursor()
-        sql = """ INSERT INTO addon (esoui_id,name,updated)
-                VALUES(?,?,?) """
+        sql = f""" INSERT INTO local_addon (esoui_id,name)
+                VALUES(?,?) """
         cur.execute(sql, addon)
         conn.commit()
         return cur.lastrowid  # returns the row id of the cursor object, the addon id
 
-
-def get_all_addons():
-    """Query all rows of addon table
+def insert_local_addon(addon):
+    """Create a new addon for table
     :param conn: The SQLite connection object
-    :return: All rows containing data from the addon table
+    :param addon: local_folder name for the addon
+    :return: addon id
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        sql = f""" INSERT INTO web_addon (folder_name)
+                VALUES(?) """
+        cur.execute(sql, (addon))
+        conn.commit()
+        return cur.lastrowid  # returns the row id of the cursor object, the addon id
+
+
+
+def get_all_web_addons():
+    """Query all rows of the web_addon table
+    :return: All rows containing data from the web_addon table
     """
     conn = create_connection()
     cur = conn.cursor()
-    cur.execute("SELECT esoui_id,name,search_name,updated,version,user_updated FROM addon")
+    cur.execute("SELECT esoui_id,name FROM web_addon")
     rows = cur.fetchall()
+    
 
-    return rows  # return the rows
-
-def get_one_addon(field, value):
-    """Query a single row of the addon table
-    :param conn: The SQLite connection object
-    :return: All rows containing data from the addon table
+def get_all_local_addons():
+    """Query all rows of the local_addon table
+    :return: All rows containing data from the local_addon table
     """
     conn = create_connection()
     cur = conn.cursor()
-    sql = f"""SELECT esoui_id,name,search_name,updated,version,user_updated,installed 
-                FROM addon
-                WHERE {field} GLOB '{value}*'"""
+    cur.execute("SELECT esoui_id,folder_name,web_name,local_version,web_version FROM local_addon")
+    rows = cur.fetchall()
+    return rows 
+
+def get_one_addon(table, field, value):
+    """Query a single row of the requested table
+    :param field: The column to search
+    :param value: The desired value of the field
+    :return: A single row from the table
+    """
+    conn = create_connection()
+    cur = conn.cursor()
+    if type(value) == int:
+         sql = f"""SELECT *
+                FROM {table}
+                WHERE {field}=={value}"""   
+    else:
+        sql = f"""SELECT *
+                    FROM {table}
+                    WHERE {field}=='{value}'"""
     cur.execute(sql)
     row = cur.fetchone()
-
     return row  # return the row
 
-def get_matching_addons(field, value):
-    """Query a single row of the addon table
-    :param conn: The SQLite connection object
-    :return: All rows containing data from the addon table
-    """
-    conn = create_connection()
-    cur = conn.cursor()
-    sql = f"""SELECT esoui_id,name,search_name,updated,version,user_updated,installed 
-                FROM addon
-                WHERE {field} GLOB '{value}*'"""
-    cur.execute(sql)
-    row = cur.fetchall()
-    return row
 
-def update_one_addon(search_field, search_name, update_field, new_data):
+def update_one_addon(table, search_field, search_name, update_field, new_data):
+    '''Update one row of the db
+    :param table: The table to update
+    :param search_field: The column to match in the WHERE clause
+    :param search_name: The value to match in the WHERE clause
+    :param update_field: The column to update
+    :param new_data: The new value to add to the column
+    :returns: True if successful, False if there was an exception'''
     conn = create_connection()
     cur = conn.cursor()
     if type(new_data) == int:
-        sql = f"""UPDATE addon SET {update_field}={new_data}, user_updated=1
+        sql = f"""UPDATE {table} SET {update_field}={new_data}, user_updated=1
                 WHERE {search_field}='{search_name}'"""
     else:
-        sql = f"""UPDATE addon SET {update_field}='{new_data}', user_updated=1
+        sql = f"""UPDATE {table} SET {update_field}='{new_data}', user_updated=1
                  WHERE {search_field}='{search_name}'"""
     try:
         cur.execute(sql)
@@ -136,30 +153,19 @@ def update_one_addon(search_field, search_name, update_field, new_data):
     except Exception as e:
         return False
 
-def update_addon_by_id(esoui_id, field, new_value):
+def update_addon_by_id(table, esoui_id, field, new_value):
+    '''Update one row of the db using the usoui_id
+    :param table: The table to update
+    :param esoui_id: The esoui_id from the website
+    :param field: The column to update
+    :param new_value: The new value to add to the column
+    :returns: True if successful, False if there was an exception'''
     conn = create_connection()
     cur = conn.cursor()
     if type(new_value) == int:
-        sql = f"""UPDATE addon SET {field}={new_value} WHERE esoui_id='{esoui_id}'"""
+        sql = f"""UPDATE {table} SET {field}={new_value} WHERE esoui_id='{esoui_id}'"""
     else:
-        sql = f"""UPDATE addon SET {field}='{new_value}' WHERE esoui_id='{esoui_id}'"""
-    try:
-        cur.execute(sql)
-        conn.commit()
-        conn.close()
-        return True
-    except Exception as e:
-        return False
-
-
-def update_addons_from_list(search_field, search_list, update_field, new_data_list):
-    pass
-
-
-def set_all_to_uninstalled():
-    conn = create_connection()
-    cur = conn.cursor()
-    sql = f"""UPDATE addon SET installed=0 WHERE installed=1"""
+        sql = f"""UPDATE {table} SET {field}='{new_value}' WHERE esoui_id='{esoui_id}'"""
     try:
         cur.execute(sql)
         conn.commit()
